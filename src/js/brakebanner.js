bb92ded000faf2c86ebaa818c07961a59031db33
wangfangyuan <862704559@qq.com>
2022-07-25 16:28:52 +0800

    feat: 创建PIXI容器

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -1,5 +1,14 @@
 class BrakeBanner{
 	constructor(selector){
-		
+		this.createApp(selector)
+	}
+	// 创建容器
+	createApp (selector){
+		this.app = new PIXI.Application({
+			width: window.innerWidth,
+			height: window.innerHeight,
+			backgroundColor: 'black'
+		})
+		document.querySelector(selector).appendChild(this.app.view)
 	}
 }
