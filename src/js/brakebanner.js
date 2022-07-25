5f60a60d87585e6d64af423fe9560e5dc4251e5d
wangfangyuan <862704559@qq.com>
2022-07-25 16:34:35 +0800

    feat: 引入图片资源

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -1,6 +1,7 @@
 class BrakeBanner{
 	constructor(selector){
 		this.createApp(selector)
+		this.loadImg()
 	}
 	// 创建容器
 	createApp (selector){
@@ -10,5 +11,22 @@ class BrakeBanner{
 			backgroundColor: 'black'
 		})
 		document.querySelector(selector).appendChild(this.app.view)
+	}
+	// 引入图片资源
+	loadImg (){
+		// const that = this
+		const loader = new PIXI.Loader();
+		this.loader = loader
+		const loaderMap = {
+			bike: 'images/brake_bike.png',
+			handlerbar: 'images/brake_handlerbar.png',
+			lever: 'images/brake_lever.png',
+			btn: 'images/btn.png',
+			btn_circle: 'images/btn_circle.png',
+		}
+		for(const key in loaderMap) {
+			loader.add(key, loaderMap[key])
+		}
+		loader.load();
 	}
 }
